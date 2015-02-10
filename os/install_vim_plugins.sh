1f157654dc40c2bc94c8b08d0d2e5abf627b76b8
Steven Irby <steven.a.irby@gmail.com>
2015-02-10 14:18:35 -0800

    Updates for me

--- os/install_vim_plugins.sh
+++ os/install_vim_plugins.sh
@@ -1,25 +1,7 @@
 #!/bin/bash
-
-cd "$(dirname "${BASH_SOURCE}")" && source "utils.sh"
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-main() {
-
-    if [ $(cmd_exists "git") -eq 0 ]; then
-
-        rm -rf ~/.vim/plugins/Vundle.vim &> /dev/null \
-            && git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/plugins/Vundle.vim &> /dev/null \
-            && printf "\n" | vim +PluginInstall +qall 2> /dev/null
-            #     └─ simulate the ENTER keypress for
-            #        the case where there are warnings
-
-        print_result $? "Install Vim plugins"
-
-    else
-        print_error "Git is required, please install it!\n"
-    fi
-
-}
-
-main
+set -e
+set -x
+cd vim
+sudo cp -av . /usr/share/vim/vimfiles/.
+sudo helpztags /usr/share/vim/vimfiles/doc
+sudo apt-get install libxml2-utils exuberant-ctags
