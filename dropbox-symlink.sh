b60ffb8429e26ba636aad3c38c050555d969b07b
Steven Irby <steven.a.irby@gmail.com>
2016-03-31 00:59:39 -0500

    update dropbox-symlink file

--- dropbox-symlink.sh
+++ dropbox-symlink.sh
@@ -2,6 +2,6 @@
 
 cd "$(dirname "${BASH_SOURCE}")";
 
-sudo rm -r ~/Documents/ ~/Documents/
+sudo rm -r ~/Downloads/ ~/Documents/
 ln -s -f ~/Dropbox/DellXPS13/Downloads/ ~/Downloads/
 ln -s -f ~/Dropbox/DellXPS13/Documents/ ~/Documents/
