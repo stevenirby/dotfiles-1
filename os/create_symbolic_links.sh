3af3ceaf234b83e2c8d791e0981066cd0b0d32c0
Steven Irby <steven.a.irby@gmail.com>
2017-05-13 16:17:08 -0500

    Revert changes

--- os/create_symbolic_links.sh
+++ os/create_symbolic_links.sh
@@ -40,8 +40,6 @@ main() {
 
         sourceFile="$(cd .. && pwd)/$i"
         targetFile="$HOME/.$(printf "%s" "$i" | sed "s/.*\/\(.*\)/\1/g")"
-        echo $targetFile
-        echo $sourceFile
 
         if [ -e "$targetFile" ]; then
             if [ "$(readlink "$targetFile")" != "$sourceFile" ]; then
@@ -49,16 +47,16 @@ main() {
                 ask_for_confirmation "'$targetFile' already exists, do you want to overwrite it?"
                 if answer_is_yes; then
                     # rm -rf "$targetFile"
-                    echo execute "ln -fs $sourceFile $targetFile" "$targetFile → $sourceFile"
+                    execute "ln -fs $sourceFile $targetFile" "$targetFile → $sourceFile"
                 else
-                    echo print_error "$targetFile → $sourceFile"
+                    print_error "$targetFile → $sourceFile"
                 fi
 
             else
-                echo print_success "$targetFile → $sourceFile"
+                print_success "$targetFile → $sourceFile"
             fi
         else
-            echo execute "ln -fs $sourceFile $targetFile" "$targetFile → $sourceFile"
+            execute "ln -fs $sourceFile $targetFile" "$targetFile → $sourceFile"
         fi
 
     done
