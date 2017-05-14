23f09c76a7dd8fbcb9bdf606f78ae2c11ff51736
Steven Irby <steven.a.irby@gmail.com>
2017-05-14 15:37:52 -0500

    Update nvm script

--- os/nvm.sh
+++ os/nvm.sh
@@ -3,7 +3,7 @@
 cd "$(dirname "${BASH_SOURCE}")" \
     && . "./utils.sh"
 
-declare -r LOCAL_SHELL_CONFIG_FILE="$HOME/.bash_profile"
+declare -r LOCAL_SHELL_CONFIG_FILE="$HOME/.bashrc"
 declare -r NVM_DIRECTORY="$HOME/.nvm"
 declare -r NVM_GIT_REPO_URL="https://github.com/creationix/nvm.git"
 
