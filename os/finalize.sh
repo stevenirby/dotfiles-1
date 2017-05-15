1fee8c368a6248cf65cc4820139dea5c0196d77f
Steven Irby <steven.a.irby@gmail.com>
2017-05-15 11:56:00 -0500

    Fix symlinks for sublime text

--- os/finalize.sh
+++ os/finalize.sh
@@ -4,18 +4,17 @@ cd "$(dirname "${BASH_SOURCE}")" && source "utils.sh"
 
 main() {
     local subl="/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl"
-    local sublime="/usr/local/bin/sublime"
     local gmvault="~/Dropbox/Backup/.gmvault"
     local alfredWorkflows="~/Dropbox/Backup/workflows"
     local vms="~/Dropbox/Backup/VirtualBox\ VMs/"
-    local sublimePackages="~/Dropbox/Backup/sublime_text/Packages/"
+    local sublimePackages="~/Dropbox/Backup/sublime_text/Installed\ Packages"
     declare -r OS="$(get_os)"
 
     # OS specific symlinks
     if [ "$OS" == "osx" ]; then
-        execute "ln -s $subl $sublime" "$subl → $sublime"
+        execute "ln -s $subl /usr/local/bin/sublime" "$subl → /usr/local/bin/sublime"
         execute "ln -s $alfredWorkflows ~/workflows" "$alfredWorkflows → ~/workflows"
-        execute "ln -s $sublimePackages ~/Library/Application Support/Sublime Text 3/ Packages" "$gmvault → ~/Library/Application Support/Sublime Text 3/ Packages"
+        execute "ln -s $sublimePackages ~/Library/Application\ Support/Sublime Text 3/Installed\ Packages" "$sublimePackages → ~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages"
     fi
 
     execute "ln -s $gmvault ~/.gmvault" "$gmvault → ~/.gmvault"
